910c33a1b79185e03387d889474ff5166755c0ac
Nezhinskiy22 <nezhinskiy22@gmail.com>
2021-09-11 13:40:37 +0300

    modal ready

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useMemo, useState } from "react";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -29,6 +30,8 @@ function App() {
     query: "",
   });
 
+  const [modal, setModal] = useState(false);
+
   const sortedPosts = useMemo(() => {
     console.log("Sorted posts");
     if (filter.sort) {
@@ -47,6 +50,7 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false);
   };
 
   // Отримуємо post з дочірнього компонента
@@ -57,8 +61,12 @@ function App() {
 
   return (
     <div className="App">
-      <MyModal></MyModal>
-      <PostForm create={createPost} />
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Створити запис
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
       <PostList
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PostItem from "./PostItem";
+import {TransitionGroup, CSSTransition} from "react-transition-group"
 
 function PostList({ posts, title, remove }) {
   if (!posts.length) {
@@ -9,14 +10,15 @@ function PostList({ posts, title, remove }) {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>{title}</h1>
+      <TransitionGroup>
       {posts.map((post, index) => (
-        <PostItem
-          remove={remove}
-          number={index + 1}
-          post={post}
-          key={posts.id}
-        />
+        <CSSTransition key={posts.id} timeout={500} classNames = "post"><PostItem
+        remove={remove}
+        number={index + 1}
+        post={post}
+      /></CSSTransition>
       ))}
+      </TransitionGroup>
     </div>
   );
 }
